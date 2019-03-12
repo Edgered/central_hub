89d6b046c5ab544dca66272f5d07580ba572b1df
zijian_pi3_bplus <zijian.huang@uwaterloo.ca>
2019-03-11 22:35:28 -0500

    - add the argparse for mqtt publisher

--- mqtt/mqtt_publisher.py
+++ mqtt/mqtt_publisher.py
@@ -1,6 +1,28 @@
 import paho.mqtt.publish as publish
+import argparse
+
+
+def main():
  
-MQTT_SERVER = "192.168.1.5"
-MQTT_PATH = "test_channel"
- 
-publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
+
+    parser = argparse.ArgumentParser(description='set input file')
+    parser.add_argument('path_name', type=str, 
+            help='select path name', default='lpu2hub')
+    parser.add_argument('payload_name', type=str, 
+            help='select the payload', default='hello from LPU')
+    parser.add_argument('server_name', type=str, 
+            help='select the server_name', default='localhost')
+    
+
+    args = parser.parse_args()
+    print("argument is ", args)
+    
+    # MQTT_SERVER = args.server_name
+    # MQTT_PATH = args.path_name
+    
+    # publish (topic, payload=None, hostname=None)
+    publish.single(args.path_name, args.payload_name, hostname=args.server_name)
+    print("publishing ends")
+    
+if __name__ == '__main__':
+    main()
